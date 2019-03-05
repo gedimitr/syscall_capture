507549ef6591e50efa7b54b6a00d2f790a82d52c
Gerasimos Dimitriadis <gedimitr@gmail.com>
2019-03-06 00:29:15 +0200

    Print rudimentary logging info on all intercepted syscalls

--- syscall_capture.c
+++ syscall_capture.c
@@ -1,12 +1,21 @@
 #include "libsyscall_intercept_hook_point.h"
 
 #include <syscall.h>
+#include <stdio.h>
+#include <string.h>
 
 static int hook(long syscall_number, long arg0, long arg1, long arg2, 
 	long arg3, long arg4, long arg5, long *result)
 {
-	const char *msg = "ping\n";
-	syscall_no_intercept(SYS_write, 1, msg, sizeof(msg));
+	static char buffer[128];
+
+	*result = syscall_no_intercept(syscall_number, arg0, arg1, arg2, arg3, arg4, arg5);
+
+	const char *msg = "System Call Number: %ld --- Arg0(%lx) Arg1(%lx) Result(%lx)\n";
+	sprintf(buffer, msg, syscall_number, arg0, arg1, *result);
+	syscall_no_intercept(SYS_write, 1, buffer, strlen(buffer));
+
+	return 0;
 }
 
 static __attribute__((constructor))
