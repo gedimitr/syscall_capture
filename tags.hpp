315156b6cd76b979b61d1eecbbe0daba12e2eb22
Gerasimos Dimitriadis <gedimitr@gmail.com>
2019-03-22 01:08:37 +0200

    Declare tag constants as inline
    So that we avoid the unnecessary creation of duplicate symbols

--- tags.hpp
+++ tags.hpp
@@ -6,15 +6,15 @@
 namespace Tag
 {
 
-constexpr uint16_t VariableHeaderPart = 0x0001;
+inline const uint16_t VariableHeaderPart = 0x0001;
 
 // uname strings
-constexpr uint16_t UnameSysname = 0x0002;
-constexpr uint16_t UnameNodename = 0x0003;
-constexpr uint16_t UnameRelease = 0x0004;
-constexpr uint16_t UnameVersion = 0x0005;
-constexpr uint16_t UnameMachine = 0x0006;
-constexpr uint16_t UnameDomainName = 0x0007;
+inline const uint16_t UnameSysname = 0x0002;
+inline const uint16_t UnameNodename = 0x0003;
+inline const uint16_t UnameRelease = 0x0004;
+inline const uint16_t UnameVersion = 0x0005;
+inline const uint16_t UnameMachine = 0x0006;
+inline const uint16_t UnameDomainName = 0x0007;
 
 }
 
