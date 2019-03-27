6f274888090ab511846ede3978c850b510dbb086
Gerasimos Dimitriadis <gedimitr@gmail.com>
2019-03-28 00:56:49 +0200

    Implement Scoped TLV writer

--- tlv.cpp
+++ tlv.cpp
@@ -1,5 +1,15 @@
 #include "tlv.hpp"
 
+namespace {
+
+ManagedBuffer &writeTag(ManagedBuffer &managed_buffer, uint16_t tag)
+{
+    managed_buffer.writeField(tag);
+    return managed_buffer;
+}
+
+}
+
 bool writeTlv(ManagedBuffer &managed_buffer, uint16_t tag, const char *inp, uint32_t num_bytes)
 {
     uint32_t space_reqs = 8 + calcBytesAfterPadding(num_bytes);
@@ -9,8 +19,9 @@ bool writeTlv(ManagedBuffer &managed_buffer, uint16_t tag, const char *inp, uint
     }
 
     if (canFitInShortTlv(num_bytes)) {
+        uint16_t num_bytes_short = static_cast<uint16_t>(num_bytes);
         managed_buffer.writeFieldBigEndianUnchecked<uint16_t>(tag);
-        managed_buffer.writeFieldBigEndianUnchecked<uint16_t>(num_bytes);
+        managed_buffer.writeFieldBigEndianUnchecked<uint16_t>(num_bytes_short);
     } else {
         managed_buffer.writeFieldBigEndianUnchecked<uint32_t>(tag);
         managed_buffer.writeFieldBigEndianUnchecked<uint32_t>(num_bytes);
@@ -21,3 +32,6 @@ bool writeTlv(ManagedBuffer &managed_buffer, uint16_t tag, const char *inp, uint
 
     return true;
 }
+ScopedTlv::ScopedTlv(ManagedBuffer &managed_buffer, uint16_t tag) :
+    m_managed_buffer(managed_buffer),
+    m_length_recorder(writeTag(managed_buffer, tag)) { }
