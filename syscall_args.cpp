63aa583e964399503e87baabdbec72792ceaaa20
Gerasimos Dimitriadis <gedimitr@gmail.com>
2019-04-19 01:30:45 +0300

    Pack integer arguments if they fit in 32bit field

--- syscall_args.cpp
+++ syscall_args.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include <tuple>
 
 #include "information_elements.hpp"
@@ -5,16 +6,31 @@
 
 #include "syscall_args.hpp"
 
+namespace {
+
+template<typename T>
+bool canFitIn(int64_t value)
+{
+    return std::numeric_limits<T>::min() <= value && value <= std::numeric_limits<T>::max();
+}
+
+}
+
 void writeSyscallArgumentInt(ManagedBuffer &managed_buffer, int64_t arg)
 {
     ScopedIE ie(managed_buffer, IETag::ArgInt);
-    managed_buffer.writeField(arg);
+    if (canFitIn<int32_t>(arg)) {
+        int32_t short_arg = static_cast<int32_t>(arg);
+        managed_buffer.writeField(short_arg);
+    } else {
+        managed_buffer.writeField(arg);
+    }
 }
 
-void writeSyscallArgumentData(ManagedBuffer &managed_buffer, int64_t arg, int32_t size)
+void writeSyscallArgumentData(ManagedBuffer &managed_buffer, int64_t arg, int64_t size)
 {
     const char *data = reinterpret_cast<const char *>(arg);
-    auto [tag, ie_length] = size > 0xffff ? std::make_tuple(IETag::ArgPartialString, 0xffff)
+    auto [tag, ie_length] = size > 0xffff ? std::make_tuple(IETag::ArgPartialString, 0xffffL)
                                           : std::make_tuple(IETag::ArgFullString, size);
     ScopedIE ie(managed_buffer, tag);
     managed_buffer.writeData(data, static_cast<uint32_t>(ie_length));
--- syscall_args.hpp
+++ syscall_args.hpp
@@ -6,6 +6,6 @@
 class ManagedBuffer;
 
 void writeSyscallArgumentInt(ManagedBuffer &managed_buffer, int64_t arg);
-void writeSyscallArgumentData(ManagedBuffer &managed_buffer, int64_t arg, int32_t size);
+void writeSyscallArgumentData(ManagedBuffer &managed_buffer, int64_t arg, int64_t size);
 
 #endif // SYSCALL_ARGS_HPP
