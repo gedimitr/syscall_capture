bb51d343cf1c98bed8dbe63da03f5b4064b51b6f
Gerasimos Dimitriadis <gedimitr@gmail.com>
2019-05-09 21:51:34 +0300

    Replace magic number with const value

--- file_writer.cpp
+++ file_writer.cpp
@@ -13,6 +13,8 @@ namespace {
 static constexpr uint32_t PAGE_SIZE = 1024 * 1024;
 static constexpr uint32_t FLUSH_THRESHOLD = 128 * 1024;
 
+static constexpr int INVALID_FD = -1;
+
 int openFileForWriting(const char *path)
 {
     int64_t res = syscall_no_intercept(SYS_open, path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
@@ -23,7 +25,7 @@ int openFileForWriting(const char *path)
 
 FileWriter::FileWriter(const Configuration &configuration) :
     m_configuration(configuration),
-    m_output_file_fd(-1),
+    m_output_file_fd(INVALID_FD),
     m_memory(static_cast<char *>(mapMemory(PAGE_SIZE))),
     m_managed_buffer(m_memory, PAGE_SIZE) { }
 
@@ -35,11 +37,11 @@ FileWriter::~FileWriter()
 
 bool FileWriter::openOutputFile()
 {
-    if (m_output_file_fd == -1) {
+    if (m_output_file_fd == INVALID_FD) {
         m_output_file_fd = openFileForWriting(m_configuration.getOutputFilePath());
     }
 
-    return (m_output_file_fd != -1);
+    return (m_output_file_fd != INVALID_FD);
 }
 
 ManagedBuffer &FileWriter::getManagedBuffer()
@@ -63,7 +65,7 @@ void FileWriter::unlock()
 
 void FileWriter::flush()
 {
-    if (m_output_file_fd != -1) {
+    if (m_output_file_fd != INVALID_FD) {
         uint32_t num_bytes = m_managed_buffer.getCurrentPosition();
         const char *data = m_managed_buffer.getRawBuffer();
 
